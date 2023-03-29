e46db12246ff8b9b760c3c732e875581c292ade8
Bartłomiej Mojek <Bartlomiej.Mojek@microsoft.wsei.edu.pl>
2023-03-29 19:36:48 +0200

    End upload every 4h

--- YtTwitch/Program.cs
+++ YtTwitch/Program.cs
@@ -10,6 +10,7 @@ using Google.Apis.Util.Store;
 using Google.Apis.YouTube.v3;
 using Google.Apis.YouTube.v3.Data;
 using System.Reflection;
+using System.Diagnostics;
 
 List<string> ListaUrl = new List<string>();
 List<string> Title = new List<string>();
@@ -19,9 +20,25 @@ List<string> TempListaUrl = new List<string>()
 {
     "https://clips-media-assets2.twitch.tv/1nIkfNbQ6GhHXhkKvDvlug/AT-cm%7C1nIkfNbQ6GhHXhkKvDvlug.mp4",
     "https://clips-media-assets2.twitch.tv/OVfA-WXzZUt7hR6gwShoyg/AT-cm%7COVfA-WXzZUt7hR6gwShoyg.mp4",
+    "https://clips-media-assets2.twitch.tv/RKq3GRvAXr7rFAGx9XunVA/AT-cm%7CRKq3GRvAXr7rFAGx9XunVA.mp4",
+    "https://clips-media-assets2.twitch.tv/GUvuj33j0iuEYAUWvjppkQ/39442963445-offset-25164.mp4",
+    "https://clips-media-assets2.twitch.tv/CojARo7Z8FNoUSkKOvSygw/39441953669-offset-1904.mp4",
+    "https://clips-media-assets2.twitch.tv/q1oaLv3pNkd3Q2je8O-GzQ/AT-cm%7Cq1oaLv3pNkd3Q2je8O-GzQ.mp4",
+    "https://clips-media-assets2.twitch.tv/DhKZS-z65ihbYIs7B1Trnw/AT-cm%7CDhKZS-z65ihbYIs7B1Trnw.mp4",
+    "https://clips-media-assets2.twitch.tv/dyswnEv7AMIhlnQPo7GRhQ/AT-cm%7CdyswnEv7AMIhlnQPo7GRhQ.mp4",
+    "https://clips-media-assets2.twitch.tv/z4SyZDQa_5DOF3Db92oBaQ/39448902037-offset-4976.mp4",
+    "https://clips-media-assets2.twitch.tv/d2ffGxHTL-fYHbhgsKDJrw/AT-cm%7Cd2ffGxHTL-fYHbhgsKDJrw.mp4",
+    "https://clips-media-assets2.twitch.tv/XGD7E3xJ7lOwCXR5yCtiOg/AT-cm%7CXGD7E3xJ7lOwCXR5yCtiOg.mp4",
+    "https://clips-media-assets2.twitch.tv/Leb5sZ4wEq-RM7fAC9oftw/AT-cm%7CLeb5sZ4wEq-RM7fAC9oftw.mp4",
+    "https://clips-media-assets2.twitch.tv/W-dSMFO5MZW3ENuxv-aZMw/AT-cm%7CW-dSMFO5MZW3ENuxv-aZMw.mp4",
+    "https://clips-media-assets2.twitch.tv/1eknT3u-axiypQjUiMUu2w/AT-cm%7C1eknT3u-axiypQjUiMUu2w.mp4",
+    "https://clips-media-assets2.twitch.tv/XXbOFIG_tUDqpKuR8wT5lg/39441953669-offset-14924.mp4",
+    "https://clips-media-assets2.twitch.tv/z9lR587i4XPvxiUKfteqZg/AT-cm%7Cz9lR587i4XPvxiUKfteqZg.mp4",
+    "https://clips-media-assets2.twitch.tv/fEdYcH4ZAYWVNDsK_01zAA/39442671845-offset-9156.mp4",
+    "https://clips-media-assets2.twitch.tv/Ptp5V00OJFFFqW0yt7eiAg/AT-cm%7CPtp5V00OJFFFqW0yt7eiAg.mp4",
+    "https://clips-media-assets2.twitch.tv/nVn1LEHwAOObGNuYCOhAhw/AT-cm%7CnVn1LEHwAOObGNuYCOhAhw.mp4",
+    "https://clips-media-assets2.twitch.tv/MNyURB8Oe0hHEgfNyss13Q/39444328549-offset-30886.mp4"
 };
-
-//DODAJ AKTUALNE CLIPY
 
 static void DownloadUrls(List<string> listaUrl, List<string> Title, List<string> StreamerName)
 {
@@ -34,34 +51,31 @@ static void DownloadUrls(List<string> listaUrl, List<string> Title, List<string>
     WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
     driver.Manage().Window.Maximize();
 
-    System.Threading.Thread.Sleep(1000);
+    System.Threading.Thread.Sleep(1200);
     IReadOnlyCollection<IWebElement> clips = wait.Until(i => i.FindElements(By.ClassName("clip-container")));
-    int i = 0;
+    int z = 0;
     foreach (IWebElement element in clips)
     {
-        if (i > 11)
+        if (z > 11)
         {
             Actions actions = new Actions(driver);
             actions.ScrollByAmount(0, 200);
             actions.Perform();
-            i = 0;
         }
-        System.Threading.Thread.Sleep(800);
+        System.Threading.Thread.Sleep(1000);
         element.Click();
-        System.Threading.Thread.Sleep(800);
+        System.Threading.Thread.Sleep(1000);
         IReadOnlyCollection<IWebElement> Dbtn = wait.Until(i => i.FindElements(By.XPath("/html/body/main/div[3]/div/div[3]/div/div[2]/div[1]/div/a")));
-        IReadOnlyCollection<IWebElement> DTitle = wait.Until(i => i.FindElements(By.CssSelector("#root > div > div.Layout-sc-1xcs6mc-0.video-player > div > div > div > div > div:nth-child(4) > div > div:nth-child(1) > div.Layout-sc-1xcs6mc-0.bZVrjx.tw-card > div > div > div.Layout-sc-1xcs6mc-0.bZVrjx.tw-card-body > div > p:nth-child(2)")));
-        IReadOnlyCollection<IWebElement> DStreamerName = wait.Until(i => i.FindElements(By.XPath("/html/body/div[1]/div/div[1]/div/div/div/div/div[4]/div/div[1]/div[2]/div/div/div[2]/div/p[2]/a[1]")));
-        if (!listaUrl.Contains(Dbtn.First().GetAttribute("href")))
-        {
-            listaUrl.Add(Dbtn.First().GetAttribute("href"));
-            //Title.Add(DTitle.First().Text);
-            //StreamerName.Add(DStreamerName.First().Text);
-        }
+        IReadOnlyCollection<IWebElement> DStreamerName = wait.Until(i => i.FindElements(By.XPath($"/html/body/main/div[3]/div/div[2]/div[1]/div[1]/div[3]/button[{z + 1}]/div[2]/div")));
+        IReadOnlyCollection<IWebElement> DTitle = wait.Until(i => i.FindElements(By.XPath($"/html/body/main/div[3]/div/div[2]/div[1]/div[1]/div[3]/button[{z + 1}]/div[3]/div[2]/div/div[1]")));
+
+        listaUrl.Add(Dbtn.First().GetAttribute("href"));
+        StreamerName.Add(DStreamerName.First().Text);
+        Title.Add(DTitle.First().Text);
 
         IReadOnlyCollection<IWebElement> Cbtn = wait.Until(i => i.FindElements(By.XPath("//button[@class = 'absolute top-0 right-0 z-30 flex items-center justify-center w-10 h-10 focus:outline-none']")));
         Cbtn.First().Click();
-        i++;
+        z++;
     }
     driver.Close();
 }
@@ -77,27 +91,44 @@ static void ChangeResolution(string clipName)
     var x = new NReco.VideoConverter.FFMpegInput[1] { new NReco.VideoConverter.FFMpegInput(clipName) };
     ffmpeg.ConvertMedia(x, $"Res{clipName}", "mp4", convertSettings);
 }
-
-/////////////////////////////
-DownloadUrls(ListaUrl, Title, StreamerName);
 int i = 0;
-foreach (var x in ListaUrl)
+static void DownloadAndUpload(List<string> ListaUrl, List<string> Title, List<string> StreamerName, List<string> TempListaUrl, int i)
 {
-    if (!TempListaUrl.Contains(x))
+    DownloadUrls(ListaUrl, Title, StreamerName);
+
+    foreach (var x in ListaUrl)
     {
-        using (var client = new WebClient())
+        if (!TempListaUrl.Contains(x))
         {
-            client.DownloadFile(x, $"clip{i}.mp4");
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(x, $"clip{i}.mp4");
 
-            TempListaUrl.Add(x);
+                TempListaUrl.Add(x);
 
-            ChangeResolution($"clip{i}.mp4");
+                ChangeResolution($"clip{i}.mp4");
+            }
+            new UploadVideo($"{StreamerName[i]} - {Title[i]} #shorts #twitch #shoty #irl", $"Resclip{i}.mp4").Run().Wait();
+
+            break;
         }
-        //new UploadVideo("Test", $"Resclip{i}.mp4").Run().Wait();
         i++;
     }
 }
-////////////////////////
+
+Stopwatch Stopwatch = Stopwatch.StartNew();
+
+while (true)
+{
+    Console.WriteLine(Stopwatch.Elapsed);
+
+    if (Stopwatch.Elapsed.Seconds >= 4)
+    {
+        DownloadAndUpload(ListaUrl, Title, StreamerName, TempListaUrl, i);
+
+        Stopwatch = Stopwatch.StartNew();
+    }
+}
 
 public class UploadVideo
 {
@@ -131,12 +162,13 @@ public class UploadVideo
 
         var video = new Video();
         video.Snippet = new VideoSnippet();
-        video.Snippet.Title = "Default Video Title";
-        video.Snippet.Description = "Default Video Description";
+        video.Snippet.Title = Title;
+        video.Snippet.Description = "Shoty dodawane automatycznie co 4h" +
+            "Daj suba ziomek 😎";
         video.Snippet.Tags = new string[] { "tag1", "tag2" };
         video.Snippet.CategoryId = "22"; // See https://developers.google.com/youtube/v3/docs/videoCategories/list
         video.Status = new VideoStatus();
-        video.Status.PrivacyStatus = "private"; // or "private" or "public"
+        video.Status.PrivacyStatus = "public"; // or "private" or "public"
         var filePath = FilePath; // Replace with path to actual movie file.
 
         using (var fileStream = new FileStream(filePath, FileMode.Open))
